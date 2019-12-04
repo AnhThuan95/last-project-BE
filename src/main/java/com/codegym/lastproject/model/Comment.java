b85b63d4540fbf9d305aff35a31096a351ad8526
anhthuan95 <thuan.hust.95@gmail.com>
2019-12-04 16:03:46 +0700

    them model comment

---
+++ src/main/java/com/codegym/lastproject/model/Comment.java
@@ -0,0 +1,60 @@
+package com.codegym.lastproject.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long rate;
+
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Comment() {
+    }
+
+    public Comment(Long rate, String comment, User user) {
+        this.rate = rate;
+        this.comment = comment;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRate() {
+        return rate;
+    }
+
+    public void setRate(Long rate) {
+        this.rate = rate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
