3dd729ca8de9d034125473da3f11a09da9577088
anhthuan95 <thuan.hust.95@gmail.com>
2019-12-09 14:33:16 +0700

    commit

--- src/main/java/com/codegym/lastproject/model/User.java
+++ src/main/java/com/codegym/lastproject/model/User.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "user_table")
+@Table(name = "user_tables")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
